ba91ee5e359929023b305a5a127452c5a31926d2
João Pedro dos Santos Souza <joaoopedrosantos003@gmail.com>
2025-11-15 13:54:52 -0300

    Commit Inicial

---
+++ database/dbProjeto.sql
@@ -0,0 +1,39 @@
+CREATE TABLE professores
+(
+    professor_id SERIAL,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_id_professor PRIMARY KEY (professor_id),
+    CONSTRAINT un_email_professor UNIQUE (email)
+)
+
+CREATE TABLE alunos
+(
+    aluno_id SERIAL,
+    nome VARCHAR(100) NOT NULL,
+    matricula VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_id_aluno PRIMARY KEY (aluno_id),
+    CONSTRAINT un_matricula_aluno UNIQUE (matricula)
+)
+
+--1xN
+CREATE TABLE disciplinas
+(
+    disciplina_id SERIAL,
+    nome_disciplina VARCHAR(100) NOT NULL,
+    codigo_disciplina VARCHAR(100) NOT NULL,
+    professor_id INTEGER,
+    CONSTRAINT pk_id_disciplina PRIMARY KEY (disciplina_id),
+    CONSTRAINT un_codigo_disciplina UNIQUE (codigo_disciplina),
+    CONSTRAINT fk_id_professor FOREIGN KEY (professor_id) REFERENCES professores
+)
+
+--NxN
+CREATE TABLE alunos_disciplinas
+(
+    aluno_id INTEGER,
+    disciplina_id INTEGER,
+    CONSTRAINT pk_id_aluno_disciplina PRIMARY KEY (aluno_id, disciplina_id),
+    CONSTRAINT fk_id_aluno FOREIGN KEY (aluno_id) REFERENCES alunos,
+    CONSTRAINT fk_id_disciplina FOREIGN KEY (disciplina_id) REFERENCES disciplinas
+)
