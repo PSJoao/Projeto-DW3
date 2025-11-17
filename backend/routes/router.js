ce411653ab738184dae5821990f4d64c2749b06e
FelipeFSM <felipemoreira.fsm@gmail.com>
2025-11-17 09:30:03 -0300

    adição das rotas

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,6 +3,9 @@ const routerApp = express.Router();
 
 //Os Apps
 const appLogin = require('../apps/login/controller/ctlLogin');
+const appAluno = require('../apps/aluno/controller/ctlAlunos');
+const appProfessores = require('../apps/professor/controller/ctlProfessores')
+
 
 routerApp.get('/', (req, res) => {
   res.send('Olá mundo!');
@@ -11,5 +14,20 @@ routerApp.get('/', (req, res) => {
 //Rotas de Autenticação
 routerApp.post('/login', appLogin.Login);
 routerApp.post('/logout', appLogin.Logout);
+
+//Rotas de aluno
+routerApp.get('/getAllAlunos', appLogin.AutenticaJWT, appAluno.getAllAlunos);
+routerApp.post('/getAlunoByID', appLogin.AutenticaJWT, appAluno.getAlunoByID);
+routerApp.post('/insertAlunos', appLogin.AutenticaJWT, appAluno.insertAlunos);
+routerApp.post('/updateAlunos', appLogin.AutenticaJWT, appAluno.updateAlunos);
+routerApp.post('/deleteAlunos', appLogin.AutenticaJWT, appAluno.deleteAlunos);
+
+//Rotas de Professores
+routerApp.get('/getAllProfessores', appLogin.AutenticaJWT, appProfessores.getAllProfessores);
+routerApp.post('/getProfessorByID', appLogin.AutenticaJWT, appProfessores.getProfessorByID);
+routerApp.post('/insertProfessores', appLogin.AutenticaJWT, appProfessores.insertProfessores);
+routerApp.post('/updateProfessores', appLogin.AutenticaJWT, appProfessores.updateProfessores);
+routerApp.post('/deleteProfessores', appLogin.AutenticaJWT, appProfessores.deleteProfessores);
+
 
 module.exports = routerApp;
